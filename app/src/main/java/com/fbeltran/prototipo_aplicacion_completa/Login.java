3600697eb34e836bf38cef7ea1870de89f1d3a4a
breaker6 <62377366+breaker6@users.noreply.github.com>
2021-06-21 17:03:18 +0200

    Añadido pantalla de login en horizontal

--- app/src/main/java/com/fbeltran/prototipo_aplicacion_completa/Login.java
+++ app/src/main/java/com/fbeltran/prototipo_aplicacion_completa/Login.java
@@ -14,24 +14,36 @@ import java.util.Objects;
 
 public class Login extends AppCompatActivity {
     //Declaramos las variables donde introduciremos el email y el password
-    EditText email;
-    EditText password;
+    EditText emailEditText;
+    EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        email = (EditText) findViewById(R.id.emailAddress);
-        password = (EditText) findViewById(R.id.password);
+        //Cogemos las variables del layout usando su id
+        emailEditText = (EditText) findViewById(R.id.emailAddress);
+        passwordEditText = (EditText) findViewById(R.id.password);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public void DoLogin(View view) {
-        Intent i = new Intent(Login.this, MainActivity.class);
+        //Intent i = new Intent(Login.this, MainActivity.class);
+        //Convertimos las variables de login en strings para poder compararlas
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
 
-        if (Objects.equals(email, "admin")){
-            Toast.makeText(this, "Login Correcto", Toast.LENGTH_SHORT).show();
+        if (email.equals("admin")){
+            //El login es correcto
+            if (password.equals("1234")) {
+                Toast.makeText(this, "Login Correcto", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(this, "Contraseña incorrecta", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else{
+            Toast.makeText(this, "Email incorrecto", Toast.LENGTH_SHORT).show();
         }
     }
 }
